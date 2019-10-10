80ef2b4d82080e31bed94fd25f8f69f5801fb81e
Brian Plancher <plancherb1@gmail.com>
2019-10-10 15:54:38 -0400

    Add files via upload

---
+++ HTM_sample_code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// update the add function definition to:
+//     1) Have the correct return type
+//     2) Have the correct input(s) (and types)
+TBD add(TBD){
+    // 3) Add two values in it together and save that in a variable
+    // 4) return the resulting value
+}
+
+int main()
+{
+    printf("Hello Welcome to My Calculator!\n");
+
+    // 5) set up two values which we want to add
+    TBD value1 = TBD
+    TBD value2 = TBD
+    
+    // 6) then call the add function to add them together
+    TBD result = add(TBD)
+    
+    // then printf the result to display it
+    printf(TBD);
+    
+    // that's it we are done with no errors (hopefully)
+    return 0;
+}
